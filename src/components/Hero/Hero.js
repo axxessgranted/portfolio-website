484e5659e28541916a48a65db261c31b2ab360fe
Grant Kantor <58207637+axxessgranted@users.noreply.github.com>
2022-03-02 22:05:20 +0200

    Add some placeholder hero content

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Access Granted to
+        <br />
+        Grant Kantor's Portfolio
+      </SectionTitle>
+      <SectionText>
+        Software developer specialising in web and mobile applications.
+      </SectionText>
+      <Button
+        onClick={() =>
+          (window.location =
+            'https://www.linkedin.com/in/grant-kantor-692951198/')
+        }
+      >
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
