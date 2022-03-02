0a303d80d000a8056af023b0c9896b87a21a4f48
Grant Kantor <58207637+axxessgranted@users.noreply.github.com>
2022-03-02 21:41:54 +0200

    Create basic Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,51 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import {
+  Container,
+  LogoStyled,
+  NavContainerStyled,
+  SocialsContainerStyled,
+  NavLink,
+  SocialIcons,
+} from './HeaderStyles';
 
-const Header = () =>  (
-  <div>
-    Header
-  </div>
+const Header = () => (
+  <Container>
+    <LogoStyled>
+      <Link href={'/'}>
+        <a style={{ display: 'flex', alignItems: 'center', color: 'white' }}>
+          <DiCssdeck size='3rem' /> <span>axxessgranted</span>
+        </a>
+      </Link>
+    </LogoStyled>
+    <NavContainerStyled>
+      <li>
+        <Link href='#projects'>
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#tech'>
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#about'>
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </NavContainerStyled>
+    <SocialsContainerStyled>
+      {/* TODO: Open social links in a new tab */}
+      <SocialIcons href='https://github.com/axxessgranted'>
+        <AiFillGithub size='3rem' />
+      </SocialIcons>
+      <SocialIcons href='https://www.linkedin.com/in/grant-kantor-692951198/'>
+        <AiFillLinkedin size='3rem' />
+      </SocialIcons>
+    </SocialsContainerStyled>
+  </Container>
 );
 
 export default Header;
--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -17,7 +17,7 @@ export const Container = styled.div`
     grid-row-gap: 0.5rem;
   }
 `;
-export const Div1 = styled.div`
+export const LogoStyled = styled.div`
   grid-area: 1 / 1 / 2 / 2;
   display: flex;
   flex-direction: row;
@@ -26,7 +26,7 @@ export const Div1 = styled.div`
     grid-area: 1 / 1 / 2 / 3;
   }
 `;
-export const Div2 = styled.div`
+export const NavContainerStyled = styled.div`
   grid-area: 1 / 2 / 2 / 4;
   display: flex;
   justify-content: space-around;
@@ -34,7 +34,7 @@ export const Div2 = styled.div`
     grid-area: 2 / 2 / 3 / 5;
   }
 `;
-export const Div3 = styled.div`
+export const SocialsContainerStyled = styled.div`
   grid-area: 1 / 5 / 2 / 6;
   display: flex;
   justify-content: space-around;
@@ -107,18 +107,16 @@ export const NavProductsIcon = styled(IoIosArrowDropdown)`
   }
 `;
 
-
-// Social Icons 
+// Social Icons
 
 export const SocialIcons = styled.a`
-transition: 0.3s ease;
-color: white;
-border-radius: 50px;
+  transition: 0.3s ease;
+  color: white;
+  border-radius: 50px;
   padding: 8px;
-&:hover {
+  &:hover {
     background-color: #212d45;
     transform: scale(1.2);
     cursor: pointer;
-    
   }
-`
+`;
